b9067f3375a4804e2feb7767abc3eae9cc99947e
Bhavya Anil <banil@ucsc.edu>
2025-01-20 15:49:14 -0800

    displayed rocket and starfield

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -6,7 +6,7 @@ class Menu extends Phaser.Scene{
     preload(){
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
-        this.load.image('startfield', './assets/starfield.png');
+        this.load.image('starfield', './assets/starfield.png');
     }
     create(){
         this.add.text(20,20,"Rocket Patrol Menu");
