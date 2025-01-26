fe688b34188f9b766baed432e08393ed3591c19b
Bhavya Anil <banil@ucsc.edu>
2025-01-25 21:43:57 -0800

    started mods

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+// name: Bhavya Anil
+// title: Rocket Patrol: The Defenders
+// time:
+// mods: 1) create new enemy spaceship type (5 pts)
+//       2) particle explosion when rocket hits spaceship (5 pts)
+//       3) create new title screen (3 pts)
+//       4) display time remaining (3 pts)
+//       5) implement mouse control for player movement (5 pts)
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
